276aa25023d2aae95729a166b2d612271adbf44c
Makar <vsyakofazno@gmail.com>
2023-08-19 14:30:51 +0300

    change and code for new buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
 "use strict"
 
 const main = document.querySelector('.main');
-const sub = document.querySelector('.operator')
+const operators = document.querySelector('.operator')
 
 const output = document.querySelector('.output')
 
 const equals = document.getElementById('equals');
-const clear = document.getElementById('clear');
+const clear = document.querySelectorAll('.block');
+const switching = document.getElementById('switching');
 
 let a = '';
 let first = false
@@ -15,16 +16,17 @@ let second = false
 
 let operator = '';
 let result = 0;
-output.innerHTML = 0;
+output.innerHTML = result;
 
 main.addEventListener('click',(e)=>{
     let value = e.target.textContent;
+    if(value == "+/-") value = ''
 
     if(first == false){
         firstValue(value);
     };
 
-    if(a && operator){
+    if(first && operator){
         secondValue(value);
     };
 });
@@ -33,49 +35,86 @@ main.addEventListener('click',(e)=>{
         output.innerHTML = '';
         a += value
         output.innerHTML = a;
-        a = +a
     }
     function secondValue(value){
         output.innerHTML = '';
         b += value
         output.innerHTML = b;
-        b = +b;
     }
 
-sub.addEventListener('click',(e)=>{
-    operator = e.target.textContent
+operators.addEventListener('click',(e)=>{
+    operator = e.target.textContent == '=' ? '' : e.target.textContent;
     first = true;
 });
 
 equals.addEventListener('click',()=>{
     switch(operator){
         case '+':
-            result = a + b
+            result = +a + +b
             break;
         case '-':
-            result = a - b
+            result = +a - +b
             break;
         case '/':
-            result = a / b
+            result = +a / +b
             break;
         case '*':
-            result = a * b
+            result = +a * +b
             break;
     }
     output.innerHTML = result; 
-    a = result;
+    a = result.toString();
     b = '';
+    operator = '';
 });
 
-clear.addEventListener('click',()=>{
-    output.innerHTML = 0
+// switching.addEventListener('click',(e)=>{
+//     if(first == false || first && operator == false){
+//         if(a>0){
+//             a *= -1;
+//         }else if(a<0){
+//             a = Math.abs(a);
+//         }
+//     }
+//     if(first && operator){
+//         if(b>0){
+//             b *= -1;
+//         }else if(b<0){
+//             b = Math.abs(b);
+//         }
+//     }
+// });
+
+clear.forEach((item)=>{
+    item.addEventListener('click',(e)=>{
+        switch(e.target.textContent){
+            case "C":
+                subtracting();
+                break;
+            case "AC":
+                deletion();
+                break;
+        }
+    });
+});
+
+function subtracting(){   
+    if(first == false || first && operator == false){
+        a = +a.toString().slice(0,-1);
+        output.innerHTML = a;
+    } 
+    if(first && operator){
+        b = +b.toString().slice(0,-1);
+        output.innerHTML = b;
+    }   
+}
+
+function deletion(){
+    result = 0;
+    output.innerHTML = '0';
     a = '';
     first = false
     b = '';
     second = false
-
     operator = '';
-    result = 0;
-
-});
-
+}
